86b0b7fac84800b5415a3bcfefdc9f1ec7e12612
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-02-03 21:55:17 +0530

    cf

---
+++ chatroom.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int n=s.length();
+    char a[n+1];
+    strcpy(a,s.c_str());
+    set<char> a[n+1];
+    for(int i=0;i<n;i++)
+    {
+        aa[i] = a[i];
+    }
+}
