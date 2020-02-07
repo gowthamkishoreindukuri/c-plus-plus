e5cfb926b7d8a3a656e1cb67ac2232e01face135
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-02-07 20:54:12 +0530

    cf

---
+++ dollars.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int f;
+    cin >> f;
+    for(int i=0;i<f;i++)
+    {
+    int n;
+    cin >>n;
+    cout << ((n/2)+(n/3)+(n/4))<<endl;
+    }
+}
