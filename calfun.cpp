92f19d5e7b2c738505afb76e2013ea973207b560
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-02-07 20:55:16 +0530

    cf

---
+++ calfun.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n,s=0;
+    cin>>n;
+    if(n%2==0)
+    {
+        cout << n/2 <<endl;
+    }
+    else
+    {
+        cout<<(-1*((n/2)+1))<<endl;
+    }
+}
