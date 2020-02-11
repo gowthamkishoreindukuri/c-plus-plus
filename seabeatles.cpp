ca685f8c03c50182db7ee93bf81761ea10a92207
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-02-11 22:06:04 +0530

    cf

--- seabeatles.cpp
+++ seabeatles.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 int main( )
 {
-    int a1,b1,a2,b2,m=0,k;
+    int n;
+    cin >>n;
+    for(int i=0;i<n;i++)
+    {
+        int a1,b1,a2,b2;
     cin>>a1>>b1>>a2>>b2;
-    k=abs(a1-a2);
-    m=2*b1+2*b2+a1+a2+k+4;
-    cout<<m;
+if(a1==b2)
+{
+    cout << b1 <<" "<< a2<<endl;
+}
+       else
+       {
+           cout << a1 <<" "<< b2<<endl;
+       }
+
+    }
     return 0;
 }
