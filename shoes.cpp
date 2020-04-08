6a54beb1def3ccc3d035115229ecb3d3635be54e
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-04-08 21:25:50 +0530

    nml

---
+++ shoes.cpp
@@ -0,0 +1,20 @@
+/*............YOUR LIFE SHOULD BE THE ROLE MODEL FOR OTHERS................*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    long long test;
+    cin>>test;
+    while(test--)
+    {
+        int n,k;
+        cin>>n>>k;
+        string a;
+        getline(cin,a);
+        cout<<a;
+    }
+    return 0;
+}
