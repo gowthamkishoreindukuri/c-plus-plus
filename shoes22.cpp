0b1fff0140842f87b30d818248880b1c0c03b270
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-04-08 21:23:38 +0530

    nml

---
+++ shoes22.cpp
@@ -0,0 +1,15 @@
+/*............YOUR LIFE SHOULD BE THE ROLE MODEL FOR OTHERS................*/
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+        string a;
+        int n,k;
+        cin>>n>>k;
+        getline(cin,a);
+        getline(cin,a);
+        cout<<a;
+    return 0;
+}
