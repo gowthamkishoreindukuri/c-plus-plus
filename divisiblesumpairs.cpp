bc4cb30f46febb7f32582dc56fde8f71fb8ab4f2
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-08 23:57:27 +0530

    new

---
+++ divisiblesumpairs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k,c=0;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if((a[i]+a[j])%k==0)
+            {
+                c++;
+            }
+        }
+    }
+    cout<<c<<endl;
+}
