c58d961455c7d7e69ef0d90c6bf65e79fafc905f
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-09 10:45:43 +0530

    new

--- icecream.cpp
+++ icecream.cpp
@@ -1,1 +1,32 @@
-
+#incude<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        int f=0,t=0,ff=0,v,t;
+        for(int i=0;i<n;i++)
+        {
+            v=a[i]-5;
+            if(v==0)
+            {
+                f++;
+            }
+            else
+            {
+                t=v/5;
+                while()
+            }
+        }
+    }
+    return 0;
+}
