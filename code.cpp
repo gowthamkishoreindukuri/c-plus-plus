adeeb9d425ccd110da409cf8850a102c7d19d265
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-18 22:24:59 +0530

    new

---
+++ code.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long k;
+    cin>>k;
+    if(k<10000)
+    {
+    cout<<"codeforces";
+    k--;
+       while(k--)
+    {
+        cout<<"s";
+    }
+    }
+    else
+    {
+        cout<<"cccccccccccccccccccccccccoooooooooooooooodeforces";
+    k--;
+       while(k--)
+    {
+        cout<<"cccccccccccccccccccccccccoooooooooooooooodeforces";
+    }
+    }
+
+
+
+    cout<<endl;
+}
