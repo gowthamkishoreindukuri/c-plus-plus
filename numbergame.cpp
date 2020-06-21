d5d9cb35b38ee91466ab98d8732c9dcc854388ca
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-21 11:10:15 +0530

    new

--- numbergame.cpp
+++ numbergame.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int v=2;
+        while(n>1)
+        {
+            if(n>1)
+            {
+                n=n-1;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
 
