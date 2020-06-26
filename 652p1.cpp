868e02dde71498a0d2d1373abf3c11728ec9a5b8
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-26 21:55:55 +0530

    new

---
+++ 652p1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int te;
+    cin>>te;
+    while(te--)
+    {
+        int v;
+        cin>>v;
+        if(v%4!=0)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+                cout<<"YES"<<endl;
+        }
+    }
+}
