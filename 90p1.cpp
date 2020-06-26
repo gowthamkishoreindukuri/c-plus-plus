ecea0ca434cfa3c68d77262cd1ec6e6eb7600021
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-26 21:56:37 +0530

    new

---
+++ 90p1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int v1=-1,v2=-1;
+    if(a>(b/c))
+    {
+        cout<<-1<<
+    }
+    }
+    return 0;
+}
