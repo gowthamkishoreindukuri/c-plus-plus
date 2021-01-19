1a3e0ac4afe38e91af58b4a9445c3e6e7de4acb6
igkishore <gowthamkishoreindukuri@gmail.com>
2021-01-19 18:52:16 +0530

    new commit

--- v.cpp
+++ v.cpp
@@ -12,5 +12,16 @@ int main()
             cin>>a[i][j];
         }
     }
-    for(int i=0;i<n;i)
-}
+    for(int i=0;i<n;i++)
+    {
+        int cnt=0;
+        for(int j=0;j<n;j++)
+        {
+            if((i+1>=0 && i+1<=n-1 && a[i+1][j]=='o') || (i-1>=0 && i-1<=n-1 && a[i-1][j]=='o') || (j+1>=0 && j+1<=n-1 && a[i][j+1]=='o') || (j-1>=0 && j-1<=n-1 && a[i][j-1]=='o') )
+            {
+                cnt++;
+            }
+        }
+        if(cnt)
+    }
+
